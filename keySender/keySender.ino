9ba0959b54372ab440a668cb4b4e4118813334f4
Yuriy Nazarov <nazarov.yuriy.pavlovich@gmail.com>
2016-06-25 18:39:52 +0400

    Send N key while Num Lock is active.

--- keySender/keySender.ino
+++ keySender/keySender.ino
@@ -1,9 +1,20 @@
 #include <Arduino.h>
+#include "HID-Project.h"
+
+const int pinLed = LED_BUILTIN;
 
 void setup() {
-
+    pinMode(pinLed, OUTPUT);
+    BootKeyboard.begin();
 }
 
-void loop() {
 
+void loop() {
+    if (BootKeyboard.getLeds() & LED_NUM_LOCK) {
+        digitalWrite(pinLed, HIGH);
+        BootKeyboard.print('N');
+        delay(250);
+    } else {
+        digitalWrite(pinLed, LOW);
+    }
 }
